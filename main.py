b1a4666941abc911482496c0c265e55913aa8a20
Dev7 <jfgallegocardona@gmail.com>
2022-11-17 00:06:21 -0500

    Api-Gateway Metodo Login y asignacion de permisos a roles terminado guia 4.4

--- main.py
+++ main.py
@@ -8,8 +8,30 @@ import datetime
 import requests
 import re
 
+from flask_jwt_extended import create_access_token, verify_jwt_in_request
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
 app=Flask(__name__)
 cors = CORS(app)
+app.config["JWT_SECRET_KEY"]="asdfgadfgASDsdfqwert123123qeJHKJHKwrt645hbsdfhsdf75675gs"
+jwt = JWTManager(app)
+
+@app.route("/login", methods=["POST"])
+def create_token():
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-security"] + '/usuarios/validar'
+    response = requests.post(url, json=data, headers=headers)
+    if response.status_code == 200:
+        user = response.json()
+        expires = datetime.timedelta(seconds=60 * 60 * 24)
+        access_token = create_access_token(identity=user,expires_delta=expires)
+        return jsonify({"token": access_token, "user_id": user["_id"]})
+    else:
+        return jsonify({"msg": "Bad username or password"}), 401
+
 
 @app.route("/", methods=['GET'])
 def test():
